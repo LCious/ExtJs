3c4adfcc33699cd7363983333bfb9777c4b04534
LCious <mantrmn@gmail.com>
2022-09-02 10:17:24 +0300

    updt

--- ExtJs Test/classic/src/view/results/ResultController.js
+++ ExtJs Test/classic/src/view/results/ResultController.js
@@ -3,4 +3,15 @@ Ext.define('FrameworkTest.view.results.ResultController', {
 
     alias: 'controller.resultcontroller',
 
+    onPrev:function(){
+        this.doNavigation(-1);
+    },
+    doNavigation:function(index){
+        var view=this.getView();
+        currentIndex=view.indexOf(view.getActiveItem()),
+        next=currentIndex+index;
+        view.setActiveItem(next);
+        view.lookup('btnPrev').setDisabled(next==0);
+        view.lookup('btnNext').setDisabled(next==2);
+    }
 });
